f8b8e84a87861a3f0f988539b58a8efae2180019
pratulgupta2007 <pratulgupta2007@gmail.com>
2024-11-13 16:27:47 +0530

    Deleted unwanted files

--- init.bat
+++
@@ -1,7 +0,0 @@
-set url_var=0
-docker-compose up -d --build
-docker-compose exec web python manage.py migrate --noinput
-set url_var=1
-docker-compose up -d --build
-docker-compose exec web python manage.py migrate --noinput
-py ticketbooking/manage.py runserver 7000
--- test.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-import os, sys
-
-print(os.path.dirname(sys.executable))
-print(sqlite3.sqlite_version)
